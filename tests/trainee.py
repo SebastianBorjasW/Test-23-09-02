7e7fa6cbc0993b5802716904a1e1350b669eea45
Eduardo Chavez Martin <66572435+eduardohufg@users.noreply.github.com>
2024-09-25 13:53:29 -0600

    Add:dataset split

--- tests/trainee.py
+++ tests/trainee.py
@@ -33,3 +33,20 @@ img_transform = transforms.Compose([
     #y ayudar a la optimizacion(adam) 
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
+
+#creamos el dataset con las transformaciones definidas
+dataset = ImageFolder(root= rootimg, transform= img_transform)
+
+#definimos el tamaño del conjunto de entrenamiento (80%)
+train_size = int(0.8 * len(dataset))
+#definimos el tamaño del conjunto de entrenamiento (20%)
+test_val_size = len(dataset) - train_size
+
+#dividimos el dataset en conjunto de entrenamiento y conjunto de validacion y pueba
+train_dataset, test_val_dataset = random_split(dataset, [train_size, test_val_size])
+
+#dividimos el conjunto de validacion y prueba en conjunto de validacion y conjunto de prueba
+#10% y 10% del conjunto original para cada uno
+test_size = int(0.5 * len(test_val_dataset))
+val_size = len(test_val_dataset) - test_size
+test_dataset, val_dataset = random_split(test_val_dataset, [test_size, val_size])

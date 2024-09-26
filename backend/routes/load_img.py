82316920a949c72fbd99644bee3975f4cc5d2a5b
Eduardo Chavez Martin <66572435+eduardohufg@users.noreply.github.com>
2024-09-26 13:43:44 -0600

    Update: Erase load images directory API

--- backend/routes/load_img.py
+++ backend/routes/load_img.py
@@ -1,24 +1,35 @@
 import os
+import shutil
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from typing import List
 
 app = APIRouter(prefix="/api/load_img", responses={404: {"description": "Not found"}})
 
-UPLOAD_FOLDER = "uploaded_images"  # Carpeta donde se guardarán las imágenes
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Crear la carpeta si no existe
+UPLOAD_FOLDER = "uploaded_images" 
+
+
+def recreate_upload_folder():
+    
+    if os.path.exists(UPLOAD_FOLDER):
+        shutil.rmtree(UPLOAD_FOLDER)
+    
+    
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.post("/")
 async def upload_images(files: List[UploadFile] = File(...)):
+    recreate_upload_folder()
+
     saved_files = []
     
     for file in files:
         if not file.content_type.startswith("image/"):
             raise HTTPException(status_code=400, detail=f"El archivo {file.filename} no es una imagen")
         
-        # Ruta para guardar el archivo
+        
         file_location = os.path.join(UPLOAD_FOLDER, file.filename)
         
-        # Guardar el archivo en el servidor
+    
         with open(file_location, "wb") as f:
             f.write(await file.read())
         
@@ -26,6 +37,7 @@ async def upload_images(files: List[UploadFile] = File(...)):
     
     return {"message": "Imágenes recibidas correctamente", "filenames": saved_files}
 
-@app.get("/")
+@app.get("/users")
 async def users():
     return "hola"
+

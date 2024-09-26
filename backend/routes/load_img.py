eb5ba1288ce3dcb091f4dcfd99a6a8284b8d0834
Eduardo Chavez Martin <66572435+eduardohufg@users.noreply.github.com>
2024-09-26 13:08:15 -0600

    Update: API multiple images

--- backend/routes/load_img.py
+++ backend/routes/load_img.py
@@ -1,17 +1,30 @@
+import os
 from fastapi import APIRouter, UploadFile, File, HTTPException
+from typing import List
 
 app = APIRouter(prefix="/api/load_img", responses={404: {"description": "Not found"}})
 
+UPLOAD_FOLDER = "uploaded_images"  # Carpeta donde se guardarán las imágenes
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Crear la carpeta si no existe
 
 @app.post("/")
-async def upload_image(file: UploadFile = File(...)):
-    if file.content_type.startswith("image/"):
+async def upload_images(files: List[UploadFile] = File(...)):
+    saved_files = []
+    
+    for file in files:
+        if not file.content_type.startswith("image/"):
+            raise HTTPException(status_code=400, detail=f"El archivo {file.filename} no es una imagen")
         
+        # Ruta para guardar el archivo
+        file_location = os.path.join(UPLOAD_FOLDER, file.filename)
         
-        print(file.filename)
-        return {"message": "Imagen recibida correctamente", "filename": file.filename}
-    else:
-        raise HTTPException(status_code=400, detail="El archivo subido no es una imagen")
+        # Guardar el archivo en el servidor
+        with open(file_location, "wb") as f:
+            f.write(await file.read())
+        
+        saved_files.append(file.filename)
+    
+    return {"message": "Imágenes recibidas correctamente", "filenames": saved_files}
 
 @app.get("/")
 async def users():
